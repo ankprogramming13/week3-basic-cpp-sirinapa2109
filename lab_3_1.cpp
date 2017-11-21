05f81a0c5f40bf0203c7d8e4ab9e7abc67f3bc60
Unknown <sirinapahom@cvc-cha.ac.th>
2017-11-21 17:28:33 +0700

    ทำข้อ1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+
+    cout << "Miss." << "Sirinapa Homchit" << endl;
+    cout << "My nickname." << "Pray"  << endl;
+    cout << "Sex." << "Female" << endl ;
+    cout << "Birthday." << "21 September 1998" << endl;
+    cout << "Hobbies." << "Play a Game" << endl;
+    cout << "Status." << "Single" << endl;
   return 0;
 }
