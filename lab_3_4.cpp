1b5a22ccebc7b06da0eea865456f9495f2610785
Unknown <trtrinw@gmail.com>
2017-11-28 17:49:19 +0700

    ทำข้อ2-6

--- lab_3_4.cpp
+++ lab_3_4.cpp
@@ -2,7 +2,8 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
 
     //กำหนดค่าเริ่มต้น
     int a = 4, b, c, d;
@@ -19,7 +20,28 @@ int main() {
 
     // แก้ไขค่า
   	b = a * 5;
-  	c = a / 5;
+  #include <iostream>
+
+using namespace std;
+
+    int num1, num2, num3, num4, num5, sum;
+  	float avg;
+  	cout << "Enter First Number: ";
+  	cin >> num1;
+  	cout << "Enter Second Number: ";
+  	cin >> num2;
+  	cout << "Enter Third Number: ";
+  	cin >> num3;
+  	cout << "Enter Second Number: ";
+  	cin >> num4;
+  	cout << "Enter Third Number: ";
+  	cin >> num5;
+  	sum = num1 + num2 + num3 + num4 + num5;
+  	avg = (float)sum / 3;
+  	cout << "Average of the Three number is: ";
+  	cout << avg;
+  return 0;
+	c = a / 5;
   	d = a % 5;
 
     // แสดงข้อมูลหลังแก้ไข
@@ -30,6 +52,7 @@ int main() {
   	cout << d << endl;
 
     // เขียนโปรแกรมเพื่อหาค่า a = b * a + 20 * (20 - 15) / 10
+    a = b * a + 20 * (20 - 15) / 10;
 
 
     // เขียนแสดงผลค่า a
